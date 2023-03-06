337f28df75795933108937bc3663c98c421127b1
yicr <yicr@users.noreply.github.com>
2023-03-06 14:32:51 +0900

    deps: change cdk version

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.43.0',
+  cdkVersion: '2.61.0',
   defaultReleaseBranch: 'main',
   name: '@yicr/secure-log-bucket',
   description: 'secure log bucket.',
