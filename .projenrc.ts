efbb1cc22ee0da47c34fc55d6a4fabea1d557c13
yicr <yicr@users.noreply.github.com>
2023-09-15 16:57:46 +0900

    chore: min node version set to 16.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -20,7 +20,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   ],
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
-  minNodeVersion: '18.0.0',
+  minNodeVersion: '16.0.0',
   workflowNodeVersion: '18.17.1',
   depsUpgradeOptions: {
     workflowOptions: {
